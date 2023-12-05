18b72191cbad5a922db2910f01772f43a231b83e
Marek Kasztelnik <mkasztelnik@gmail.com>
2023-12-06 00:46:01 +0100

    Day 05

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,89 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(input) do
+    {seeds, mappings} = input |> AdventOfCode.Day05.Parser.parse()
+
+    seeds
+    |> Enum.map(fn seed -> map(seed, mappings) end)
+    |> Enum.min()
   end
 
-  def part2(_args) do
+  defp map(nr, steps) do
+    steps
+    |> Enum.reduce(nr, fn mappings, current ->
+      case Enum.find(mappings,  fn {range, _} -> current in range end) do
+        nil -> current
+        {_, diff} -> current + diff
+      end
+    end)
+  end
+
+  def part2(input) do
+    {seeds, mappings} = input |> AdventOfCode.Day05.Parser.parse()
+
+    ranges = seeds
+            |> Enum.chunk_every(2)
+            |> Enum.map(fn [from, size] -> from..(from + size - 1) end)
+
+    min.._ =
+      Enum.reduce(mappings, ranges, &map_ranges/2)
+      |> Enum.min_by(fn from.._ -> from end)
+
+    min
+  end
+
+  def map_ranges(mappers, ranges, splitted \\ [])
+  def map_ranges([], ranges, splitted), do: ranges ++ splitted
+  def map_ranges([{mapper, diff} | todo_mappers], ranges, splitted) do
+    {mapped, todos} =
+      Enum.reduce(ranges, {[], []}, fn range, {acc_mapped, acc_todos} ->
+        {m, t} = split_range(mapper, range)
+        {
+          Enum.map(m, fn s..e -> (s + diff)..(e + diff) end) ++ acc_mapped,
+          t ++ acc_todos
+        }
+      end)
+    map_ranges(todo_mappers, todos, mapped ++ splitted)
+  end
+
+  def split_range(ms..me, rs..re = range) when ms <= rs and me >= re, do: {[range], []}
+  def split_range(ms..me = mapper, rs..re) when ms > rs and me < re, do: {[mapper], [rs..(ms - 1 ), (me + 1)..re]}
+  def split_range(ms..me, rs..re) when ms <= rs and me >= rs and me < re, do: {[rs..me], [(me + 1)..re]}
+  def split_range(ms..me, rs..re) when ms > rs and ms <= re and me >= re, do: {[ms..re], [rs..(ms - 1)]}
+  def split_range(_, range), do: {[], [range]}
+end
+
+defmodule AdventOfCode.Day05.Parser do
+  import NimbleParsec
+
+  seeds = ignore(string("seeds: "))
+          |> repeat(integer(min: 1) |> ignore(ascii_string([?\s], min: 0)))
+          |> tag(:seeds)
+
+  mapping = repeat(integer(min: 1) |> ignore(ascii_string([?\s], min: 0)))
+            |> ignore(ascii_string([?\n], min: 1))
+  mappings = repeat(wrap(mapping))
+
+  from_to_map = ignore(ascii_string([?\n], min: 0))
+        |> (ascii_string([?a..?z], min: 1))
+        |> ignore(string("-to-"))
+        |> (ascii_string([?a..?z], min: 1))
+        |> ignore(string(" map:\n"))
+        |> wrap(mappings)
+        |> tag(:map)
+
+  defparsec :almanac, seeds |> repeat(from_to_map)
+
+  def parse(input) do
+    {:ok, [{:seeds, seeds} | map], "", _, _, _} = almanac(input)
+    map = map |> Enum.map(fn {:map, [_, _, mappings]} -> to_ranges(mappings) end)
+
+    {seeds, map}
+  end
+
+  defp to_ranges(mappings) do
+    mappings
+    |> Enum.map(fn [to, from, size] ->
+      {from..(from + size - 1), to - from}
+    end)
   end
 end
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -3,19 +3,60 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  seeds: 79 14 55 13
 
-    assert result
+  seed-to-soil map:
+  50 98 2
+  52 50 48
+
+  soil-to-fertilizer map:
+  0 15 37
+  37 52 2
+  39 0 15
+
+  fertilizer-to-water map:
+  49 53 8
+  0 11 42
+  42 0 7
+  57 7 4
+
+  water-to-light map:
+  88 18 7
+  18 25 70
+
+  light-to-temperature map:
+  45 77 23
+  81 45 19
+  68 64 13
+
+  temperature-to-humidity map:
+  0 69 1
+  1 0 69
+
+  humidity-to-location map:
+  60 56 37
+  56 93 4
+  """
+
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 35
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "split range" do
+    assert split_range(10..20, 12..13) == {[12..13], []}
+    assert split_range(10..20, 21..22) == {[], [21..22]}
+    assert split_range(10..20, 1..9) == {[], [1..9]}
+    assert split_range(10..20, 1..10) == {[10..10], [1..9]}
+    assert split_range(10..20, 20..30) == {[20..20], [21..30]}
+    assert split_range(10..20, 1..30) == {[10..20], [1..9, 21..30]}
+  end
 
-    assert result
+  test "part2" do
+    result = part2(@input)
+
+    assert result == 46
   end
 end

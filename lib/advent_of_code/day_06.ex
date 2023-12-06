b4d771e1dae39411b85967e8ed76d3561925aa8c
Marek Kasztelnik <mkasztelnik@gmail.com>
2023-12-06 14:14:00 +0100

    Small day 06 refactoring

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,20 +1,28 @@
 defmodule AdventOfCode.Day06 do
   def part1(input) do
+    calculate(input, fn numbers -> Enum.map(numbers, &String.to_integer/1) end)
+  end
+
+  def part2(input) do
+    calculate(input, fn numbers -> [Enum.join(numbers) |> String.to_integer()] end)
+  end
+
+  defp calculate(input, mapping_function) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
       [_ | numbers] = String.split(line, ~r/\s+/)
-      Enum.map(numbers, &to_i/1)
+      mapping_function.(numbers)
     end)
     |> Enum.zip()
     |> Enum.map(&count_wins/1)
-    |> Enum.reduce(1, fn wins, acc -> acc * wins end)
+    |> Enum.product()
   end
 
   defp count_wins({time, distance}) do
     delta = :math.sqrt(time * time - 4 * distance)
-    x1 = (-time - delta) / -2
-    x2 = (-time + delta) / -2
+    x1 = (time + delta) / 2
+    x2 = (time - delta) / 2
 
     to_floor(x1) - to_ceil(x2) + 1
   end
@@ -27,22 +35,5 @@ defmodule AdventOfCode.Day06 do
   defp to_floor(x) do
     truncated = trunc(x)
     if truncated == x, do: truncated - 1, else: floor(x)
-  end
-
-  defp to_i(str) do
-    {nr, _} = Integer.parse(str)
-    nr
-  end
-
-  def part2(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      [_ | numbers] = String.split(line, ~r/\s+/)
-      [Enum.join(numbers) |> to_i()]
-    end)
-    |> Enum.zip()
-    |> Enum.map(&count_wins/1)
-    |> Enum.reduce(1, fn wins, acc -> acc * wins end)
   end
 end

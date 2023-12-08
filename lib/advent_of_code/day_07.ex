e73059b40d732b05864ed40b64083b218a37aeeb
Marek Kasztelnik <mkasztelnik@gmail.com>
2023-12-08 01:53:57 +0100

    day 07

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,69 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> solve()
   end
 
-  def part2(_args) do
+  defp ranking([5]), do: 7
+  defp ranking([4, 1]), do: 6
+  defp ranking([3, 2]), do: 5
+  defp ranking([3, 1, 1]), do: 4
+  defp ranking([2, 2, 1]), do: 3
+  defp ranking([2, 1, 1, 1]), do: 2
+  defp ranking([1, 1, 1, 1, 1]), do: 1
+
+  defp score([a, b, c, d, e]), do: a * 100000000 + b * 1000000 + c * 10000 + d * 100 + e
+
+  defp to_number(?A), do: 14
+  defp to_number(?K), do: 13
+  defp to_number(?Q), do: 12
+  defp to_number(?J), do: 11
+  defp to_number(?T), do: 10
+  defp to_number(?9), do: 9
+  defp to_number(?8), do: 8
+  defp to_number(?7), do: 7
+  defp to_number(?6), do: 6
+  defp to_number(?5), do: 5
+  defp to_number(?4), do: 4
+  defp to_number(?3), do: 3
+  defp to_number(?2), do: 2
+  defp to_number(?*), do: 1
+
+  defp to_numbers(list, nil), do: list
+  defp to_numbers(list, 5), do: [5 | list]
+  defp to_numbers([first | rest], jokers), do: [first + jokers | rest]
+
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.replace(line, "J", "*") end)
+    |> solve()
+  end
+
+  defp solve(lines) do
+    lines
+    |> Enum.map(fn line ->
+      [hand, bid] = String.split(line, " ")
+      numbers =
+        hand
+        |> String.to_charlist()
+        |> Enum.map(&to_number/1)
+
+      map =
+        hand
+        |> String.to_charlist()
+        |> Enum.reduce(%{}, fn ch, acc ->
+          Map.update(acc, ch, 1, fn v -> v + 1 end)
+        end)
+
+      {jokers, map} = Map.get_and_update(map, ?*, fn _ -> :pop end)
+      type = map |> Map.values() |> Enum.sort(:desc) |> to_numbers(jokers)
+
+      {hand, type, numbers, String.to_integer(bid)}
+    end)
+    |> Enum.sort_by(fn {_, type, numbers, _,} -> ranking(type) * 10000000000 + score(numbers) end)
+    |> Enum.reduce({0, 1}, fn {_, _, _, bid}, {score, rank} -> {score + rank * bid, rank + 1} end)
+    |> elem(0)
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,51 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
     result = part1(input)
 
-    assert result
+    assert result == 6440
   end
 
-  @tag :skip
+  test "part1 extra" do
+    input = """
+    AAAAA 2
+    22222 3
+    AAAAK 5
+    22223 7
+    AAAKK 11
+    22233 13
+    AAAKQ 17
+    22234 19
+    AAKKQ 23
+    22334 29
+    AAKQJ 31
+    22345 37
+    AKQJT 41
+    23456 43
+    """
+    result = part1(input)
+
+    assert result == 1343
+  end
+
   test "part2" do
-    input = nil
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
     result = part2(input)
 
-    assert result
+    assert result == 5905
   end
 end

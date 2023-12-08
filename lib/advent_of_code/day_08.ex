741c892cbcb7b50faca159194c128ea01ed016bc
Marek Kasztelnik <mkasztelnik@gmail.com>
2023-12-08 12:22:53 +0100

    day 08

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,43 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  def part1(input) do
+    {instructions, map} = parse(input)
+
+    calc_steps(map, instructions, "AAA", fn current -> current == "ZZZ" end)
   end
 
-  def part2(_args) do
+  defp calc_steps(map, instructions, starting_point, end_condition) do
+    Enum.reduce_while(Stream.cycle(instructions), {starting_point, 0}, fn instruction, {current, score} ->
+      if end_condition.(current),
+        do: {:halt, score},
+      else: {:cont, {get(Map.get(map, current), instruction), score + 1}}
+    end)
+  end
+
+  defp get({left, _}, ?L), do: left
+  defp get({_, right}, ?R), do: right
+
+  defp parse(input) do
+    [instructions | map] = input |> String.split("\n", trim: true)
+    instructions = instructions |> String.to_charlist()
+
+    map =
+      map
+      |> Enum.map(fn line ->
+        [[start], [left], [right]] = Regex.scan(~r/\w+/, line)
+
+        {start, {left, right}}
+      end)
+      |> Map.new()
+
+    {instructions, map}
+  end
+
+  def part2(input) do
+    {instructions, map} = parse(input)
+    starting_points = map |> Map.keys() |> Enum.filter(fn s -> String.at(s, 2) == "A" end)
+
+    starting_points
+    |> Enum.map(fn start -> calc_steps(map, instructions, start, fn s -> String.at(s, 2) == "Z" end) end)
+    |> Enum.reduce(1, fn nr, acc -> div(acc * nr, Integer.gcd(acc, nr)) end)
   end
 end
--- test/advent_of_code/day_08_test.exs
+++ test/advent_of_code/day_08_test.exs
@@ -3,19 +3,52 @@ defmodule AdventOfCode.Day08Test do
 
   import AdventOfCode.Day08
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  test "part1 case1" do
+    input = """
+    RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)
+    """
     result = part1(input)
 
-    assert result
+    assert result == 2
   end
 
-  @tag :skip
+  test "part1 case2" do
+    input = """
+    LLR
+
+    AAA = (BBB, BBB)
+    BBB = (AAA, ZZZ)
+    ZZZ = (ZZZ, ZZZ)
+    """
+    result = part1(input)
+
+    assert result == 6
+  end
+
   test "part2" do
-    input = nil
+    input = """
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 6
   end
 end

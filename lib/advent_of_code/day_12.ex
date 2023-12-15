653648de9ba79b603ec697c937ee7a42dacf827e
Marek Kasztelnik <mkasztelnik@gmail.com>
2023-12-15 11:11:32 +0100

    d12

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,7 +1,61 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_args) do
+  def part1(input) do
+    parse(input)
+    |> Enum.map(&solve/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def solve_folded({row, conditions}) do
+    row_unfolded = (for _ <- 1..5, do: row) |> Enum.join("?")
+    conditions_unfolded = Enum.reduce(1..5, [], fn _, acc -> conditions ++ acc end)
+    solve({row_unfolded, conditions_unfolded})
+  end
+
+  def solve({row, conditions}) do
+    solve(row, ".", conditions)
+  end
+  def solve("", _, [0]), do: 1
+  def solve("", _, []), do: 1
+  def solve("", _, _), do: 0
+
+  def solve("#" <> rest, _, [h | t]), do: solve(rest, "#", [h - 1 | t])
+
+  def solve("." <> rest, "#", [0 | t]), do: solve(rest, ".", t)
+  def solve("." <> _rest, "#", _), do: 0
+  def solve("." <> rest, ".", t), do: solve(rest, ".", t)
+
+  def solve("?" <> rest, _, []), do: solve(rest, ".", [])
+  def solve("?" <> rest, "#", [0 | t]), do: solve(rest, ".", t)
+  def solve("?" <> rest, "#", [h | t]), do: solve(rest, "#", [h - 1 | t])
+  def solve("?" <> rest, ".", [h | t]) do
+    cache({rest, [h | t]}, fn ->
+      solve(rest, "#", [h - 1 | t]) + solve(rest, ".", [h | t])
+    end)
+  end
+  def solve(_, _, []), do: 0
+
+  def cache(key, fun) do
+    case Process.get(key) do
+      nil -> fun.() |> tap(&Process.put(key, &1))
+      v -> v
+    end
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [row, conditions] = String.split(line, " ")
+      {
+        row,
+        conditions |> String.split(",") |> Enum.map(&String.to_integer/1)
+      }
+    end)
+  end
+
+  def part2(input) do
+    parse(input)
+    |> Enum.map(&solve_folded/1)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/day_12_test.exs
+++ test/advent_of_code/day_12_test.exs
@@ -3,19 +3,43 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+    ???.### 1,1,3
+    .??..??...?##. 1,1,3
+    ?#?#?#?#?#?#?#? 1,3,1,6
+    ????.#...#... 4,1,1
+    ????.######..#####. 1,6,5
+    ?###???????? 3,2,1
+    """
 
-    assert result
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 21
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "solve" do
+    assert solve({"#", [1]}) == 1
+    assert solve({"#", [1, 2]}) == 0
+    assert solve({"##?#", [4]}) == 1
+    assert solve({"....##.....#....", [2, 1]}) == 1
+    assert solve({"#.#.###", [1, 1, 3]}) == 1
+    assert solve({"???.###", [1, 1, 3]}) == 1
+    assert solve({".??..??...?##.", [1, 1, 3]}) == 4
+    assert solve({"?#?#?#?#?#?#?#?", [1, 3, 1, 6]}) == 1
+    assert solve({"????.#...#...", [4,1,1]}) == 1
+    assert solve({"????.######..#####.", [1, 6, 5]}) == 4
+    assert solve({"?###????????", [3, 2, 1]}) == 10
+  end
 
-    assert result
+  test "solve folded" do
+    assert solve_folded({"???.###", [1, 1, 3]}) == 1
+    assert solve_folded({".??..??...?##.", [1, 1, 3]}) == 16384
+  end
+
+  test "part2" do
+    result = part2(@input)
+
+    assert result == 525152
   end
 end
